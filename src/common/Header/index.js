9a0f0e9e5e2b7399ca2c42450f4033a8b22dcd70
Adrian Bogdan <enu999@o2.pl>
2024-12-02 23:54:08 +0100

    Updates

--- src/common/Header/index.js
+++
@@ -1,7 +0,0 @@
-import { MainHeader } from "./styled";
-
-const Header = ({ title }) => (
-  <MainHeader className="header">{title}</MainHeader>
-);
-
-export default Header;
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-export const toFrom = () => "/start";
-export const toProces = () => "/proces";
-export const toTea = () => "/herbata";
-
-export const toWelcome = () => "/welcome";
-
-export const toStarted = ({ id } = { id: ":id" }) => `/start/${id}`;
