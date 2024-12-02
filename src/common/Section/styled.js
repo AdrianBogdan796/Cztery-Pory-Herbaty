2dc7ba059f0cbe87450d255695017cc643799849
Adrian Bogdan <enu999@o2.pl>
2024-12-02 21:29:14 +0100

    styled

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -5,13 +5,15 @@ export const MainSection = styled.section`
   background-color: white;
 `;
 
-export const SectionHeader = styled.h2`
-  text-align: center;
+export const SectionHeader = styled.h3`
+  display: flex;
+  justify-content: space-between;
   padding: 20px;
   font-size: 22px;
   font-weight: bolder;
   margin: 0;
   border-bottom: 1px solid rgba(128, 128, 128, 0.25);
+
   @media (max-width: 768px) {
     flex-direction: column;
   }
